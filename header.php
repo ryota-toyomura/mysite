c8381fc4ac302115abde2d52d1a65bfbf2ad0782
ryotatoyomura <miitoyo291@gmail.com>
2023-04-18 08:55:39 +0900

    4/18作業分

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="l-header l-container">
+        <div class="l-header__main">
+            <h1 class="l-header__logo">
+                Toyomii
+            </h1>
+            <nav class="l-header__nav">
+                <ul class="l-header__lists">
+                    <li class="l-header__item">
+                        <a href="" class="l-header__link">
+                            About
+                        </a>
+                    </li>
+                    <li class="l-header__item">
+                        <a href="" class="l-header__link">
+                            Blog
+                        </a>
+                    </li>
+                    <li class="l-header__item">
+                        <a href="" class="l-header__link">
+                            Works
+                        </a>
+                    </li>
+                    <li class="l-header__item">
+                        <a href="" class="l-header__link">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>

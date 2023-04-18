6bffc093cc97c9635d4d0bf588f6b987d6ad4989
ryota-toyomura <miitoyo291@gmail.com>
2023-04-18 18:14:34 +0900

    4/18作業分(会社) functions.php 3~7行目にキャッシュ制御する記述を追加

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+
+header('Expires: Tue, 1 Jan 2000 00:00:00 GMT');
+header('Last-Modified:' . gmdate( 'D, d M Y H:i:s' ) . 'GMT');
+header('Cache-Control:no-cache,no-store,must-revalidate,max-age=0');
+header('Cache-Control:pre-check=0,post-check=0',false);
+header('Pragma:no-cache');
+
 function wp_setup() //neko_theme_setupは任意の関数
 {
     add_theme_support('title-tag'); //特定の機能をテーマで使えるようにするための関数 この場合はタイトルを表示

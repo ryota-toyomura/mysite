1f2e0f64b79859e42d5ff6f81939b2d3df5bf954
ryota-toyomura <miitoyo291@gmail.com>
2023-04-18 18:14:57 +0900

    4/18その他作業分(会社

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 <main>
-
+    <div id="mainVisual">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/dummy-main.png" alt="">
+    </div>
 </main>
 <?php get_footer(); ?>
